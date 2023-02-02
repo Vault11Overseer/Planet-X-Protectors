9532301be141e7ea7219ab878558a1287b842f55
AlteredArt <cherokeedreemer@gmail.com>
2023-02-02 12:01:07 -0700

    change size of platform

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ import { UI } from './js/UI.js';
 window.addEventListener('load', function(){
   const canvas = document.getElementById('canvas1');
   const ctx = canvas.getContext('2d');
-  canvas.width = 900;
+  canvas.width = 500;
   canvas.height = 500;
 
 
