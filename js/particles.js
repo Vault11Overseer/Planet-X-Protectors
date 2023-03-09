ed675655a7210cbb23738b7bd6281545a141565f
AlteredArt <cherokeedreemer@gmail.com>
2023-03-09 16:29:36 -0700

    update

--- js/particles.js
+++ js/particles.js
@@ -1,9 +1,9 @@
 class Particle {
+
     constructor(game){
         this.game = game;
         this.markedForDeletion = false;
     }
-
 
     update(){
         this.x -= this.speedX + this.game.speed;
