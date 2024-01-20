19e4f15505849433a926d8773fc2f260d75f4f5f
AlteredArt <cherokeedreemer@gmail.com>
2024-01-19 21:58:50 -0700

    adding collision

--- invaders/invaders.js
+++ invaders/invaders.js
@@ -191,6 +191,21 @@ class Game {
             if (this.projectilesPool[i].free) return this.projectilesPool[i];
         }
     }
+
+    // COLLISION
+    checkCollision(a,b){
+        if(
+            a.x < b.x + b.width &&
+            a.x + a.width > b.x &&
+            a.y < b.y + b.height &&
+            a.y + a.height > b.y
+        ) {
+            // COLLISION
+
+        } else {
+            // NO COLLISION
+        }
+    }
 }
 
 
