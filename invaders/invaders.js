ee0148a12b3f36ee7ee7ff5440922f5d20cca3f0
AlteredArt <cherokeedreemer@gmail.com>
2024-02-01 19:08:48 -0700

    add audio

--- invaders/invaders.js
+++ invaders/invaders.js
@@ -164,7 +164,7 @@ class Beetlemorph extends Enemy{
 }
 
 class Wave {
-    
+
     constructor(game){
         this.game = game;
         this.width = this.game.columns * this.game.enemySize;
@@ -182,10 +182,12 @@ class Wave {
         if(this.y < 0)this.y += 5;
         this.speedY =0;
         // context.strokeRect(this.x, this.y, this.width, this.height)
+
         if(this.x < 0 || this.x > this.game.width -this.width){
             this.speedX *= -1;
             this.speedY = this.game.enemySize;
         }
+
         this.x += this.speedX;
         this.y += this.speedY;
         this.enemies.forEach(enemy => {
@@ -208,6 +210,7 @@ class Wave {
 }
 
 class Game {
+    
     constructor(canvas){
         this.canvas = canvas;
         this.width = this.canvas.width;
